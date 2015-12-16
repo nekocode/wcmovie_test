335510d23c06e371ad29c08e100747b519232f2f
nekocode <syfyw@qq.com>
2015-12-16 19:32:16 +0800

    finished admin

--- handler/admin/answer.py
+++ handler/admin/answer.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # coding:utf-8
+import json
+import uuid
+import MySQLdb
+from qiniu import Auth, put_data
 
 from tornado import escape
-from tornado.web import authenticated
+from tornado.web import authenticated, os
 from base import BaseHandler, pagination
+import config
 from optsql.db import db
 
 
@@ -11,65 +16,130 @@ class AnswerQueryHandler(BaseHandler):
     def data_received(self, chunk):
         pass
 
-    @authenticated
     def get(self):
-        uid = int(escape.xhtml_escape(self.current_user))
-        app_id = int(self.get_argument('app_id', 1))
-        app = db.get('SELECT * FROM app WHERE id=%d' % app_id)
+        app_id = int(self.get_argument('app_id',  int(self.get_cookie('app_id', 1))))
+        apps = db.query("SELECT * FROM app")
 
-        if app is None or app.uid != uid:
-            self.write(u'你没有权限访问该 APP')
+        if len(apps) == 0:
+            self.write(u'你还未创建任何 APP')
             return
 
-        p = int(self.get_argument('p', 1))
-        rows = pagination.get_page_rows(p, 'answer', 'WHERE app_id=%d' % app_id)
+        finded = False
+        for app in apps:
+            if app.id == app_id:
+                finded = True
+                break
+
+        if finded is False:
+            app_id = apps[0].id
+
+        self.set_cookie('app_id', str(app_id))
+        rows = db.query("SELECT * FROM answer WHERE app_id=%d" % app_id)
+        self.render('admin/answers.html', app_id=app_id, apps=apps, rows=rows)
 
 
 class AnswerEditHandler(BaseHandler):
     def data_received(self, chunk):
         pass
 
-    @authenticated
     def get(self, action):
-        uid = int(escape.xhtml_escape(self.current_user))
-        answer_id = int(self.get_argument('answer_id', 1))
-        answer = db.get('SELECT * FROM answer WHERE id=%d' % answer_id)
-        app = db.get('SELECT * FROM app WHERE id=%d' % answer.app_id)
-
-        if app is None or app.uid != uid:
-            self.write(u'你没有权限访问该 APP')
-            return
-
         if action == 'add':
-            pass
-        elif action == 'edit':
-            pass
+            app_id = int(self.get_argument('app_id',  int(self.get_cookie('app_id', -1))))
+            if app_id == -1:
+                self.write(u'缺少 app_id 参数')
+                return
 
-    @authenticated
+            self.render('admin/answer_edit.html', action=action)
+
+        elif action == 'edit':
+            _id = int(self.get_argument('id', 1))
+            answer = db.get('SELECT * FROM answer WHERE id=%d' % _id)
+
+            if answer is None:
+                self.write(u'找不到该答案')
+                return
+
+            self.render('admin/answer_edit.html', action=action, answer=answer)
+
+        elif action == 'delete':
+            _id = int(self.get_argument('id', 1))
+            answer = db.get('SELECT * FROM answer WHERE id=%d' % _id)
+
+            if answer is None:
+                self.write(json.dumps(dict(flag=False, message='未找到该答案')))
+                return
+
+            sql = "DELETE FROM answer WHERE id=%d" % _id
+            db.delete(sql)
+
+            self.write("删除成功")
+
     def post(self, action):
-        uid = int(escape.xhtml_escape(self.current_user))
-        answer_id = int(self.get_argument('answer_id', 1))
-        answer = db.get('SELECT * FROM answer WHERE id=%d' % answer_id)
-        app = db.get('SELECT * FROM app WHERE id=%d' % answer.app_id)
-
-        if app is None or app.uid != uid:
-            self.write(u'你没有权限访问该 APP')
-            return
+        self.set_header('Content-Type', 'text/json')
 
         if action == 'add':
-            pass
+            app_id = int(self.get_argument('app_id', -1))
+            if app_id == -1:
+                self.write(u'缺少 app_id 参数')
+                return
+
+            logo_url = self.get_file_uploaded_url('logo_url')
+            if logo_url is None:
+                self.write(json.dumps(dict(flag=False, message='图片上传到七牛失败')))
+                return
+
+            sql = "INSERT INTO answer(app_id, logo_url, title, subtitle, content) " \
+                  "VALUES(%d, '%s', '%s', '%s', '%s')" \
+                  % (app_id,
+                     MySQLdb.escape_string(logo_url),
+                     MySQLdb.escape_string(self.get_body_argument('title')),
+                     '',
+                     MySQLdb.escape_string(self.get_body_argument('content')),)
+
+            db.insert(sql)
+            self.write(json.dumps(dict(flag=True, url='/test/admin/answer?app_id=%d' % app_id)))
+
         elif action == 'edit':
-            pass
+            _id = int(self.get_argument('id', 1))
+            answer = db.get('SELECT * FROM answer WHERE id=%d' % _id)
 
+            if answer is None:
+                self.write(u'找不到该答案')
+                return
 
-class AnswerCountPageHandler(BaseHandler):
-    def data_received(self, chunk):
-        pass
+            logo_url = self.get_file_uploaded_url('logo_url')
+            if logo_url is None:
+                self.write(json.dumps(dict(flag=False, message='图片上传到七牛失败')))
+                return
 
-    @authenticated
-    def get(self):
-        pass
+            sql = "UPDATE answer SET logo_url='%s', title='%s', " \
+                  "subtitle='%s', content='%s' WHERE id=%d" \
+                  % (MySQLdb.escape_string(logo_url),
+                     MySQLdb.escape_string(self.get_body_argument('title')),
+                     "",
+                     MySQLdb.escape_string(self.get_body_argument('content')),
+                     _id)
 
+            db.update(sql)
+            self.write(json.dumps(dict(flag=True, url='/test/admin/answer')))
 
+    def get_file_uploaded_url(self, name):
+        if name in self.request.files:
+            fileinfo = self.request.files[name][0]
+            fname = fileinfo['filename']
+            body = fileinfo['body']
 
+            extn = os.path.splitext(fname)[1]
+            cname = str(uuid.uuid4()) + extn
 
+            q = Auth(config.QINIU_AK, config.QINIU_SK)
+            key = 'upload/' + cname
+            token = q.upload_token(config.QINIU_BUCKET_NAME)
+            ret, info = put_data(token, key, body)
+
+            if info.status_code == 200:
+                return 'http://7xon5f.com2.z0.glb.qiniucdn.com/' + key
+            else:
+                return None
+
+        return ''
--- handler/admin/app.py
+++ handler/admin/app.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # coding:utf-8
+import json
+import os
+import uuid
+import MySQLdb
+from qiniu import Auth, put_data
 
 from tornado import escape
-from tornado.web import authenticated
-from base import BaseHandler, pagination
+from base import BaseHandler
+import config
 from optsql.db import db
 
 
@@ -12,8 +17,7 @@ class AppQueryHandler(BaseHandler):
         pass
 
     def get(self):
-        p = int(self.get_argument('p', 1))
-        rows = pagination.get_page_rows(p, 'app')
+        rows = db.query("SELECT * FROM app")
 
         self.render('admin/apps.html', rows=rows)
 
@@ -23,29 +27,113 @@ class AppEditHandler(BaseHandler):
         pass
 
     def get(self, action):
-        app_id = int(self.get_argument('app_id', 1))
-        app = db.get('SELECT * FROM app WHERE id=%d' % app_id)
-
-        if app is None:
-            self.write(u'找不到该 APP')
-            return
-
         if action == 'add':
-            self.render('admin/app_edit.html')
+            self.render('admin/app_edit.html', action=action)
+
         elif action == 'edit':
-            self.render('admin/app_edit.html')
+            app_id = int(self.get_argument('app_id', 1))
+            app = db.get('SELECT * FROM app WHERE id=%d' % app_id)
+
+            if app is None:
+                self.write(u'找不到该 APP')
+                return
+
+            self.render('admin/app_edit.html', action=action, app=app)
+
+        elif action == 'delete':
+            app_id = int(self.get_argument('app_id', 1))
+            app = db.get('SELECT * FROM app WHERE id=%d' % app_id)
+
+            if app is None:
+                self.write(json.dumps(dict(flag=False, message='未找到该 APP')))
+                return
+
+            sql = "DELETE FROM app WHERE id=%d" % app_id
+            db.delete(sql)
+
+            self.write("删除成功")
 
     def post(self, action):
-        app_id = int(self.get_argument('app_id', 1))
-        app = db.get('SELECT * FROM app WHERE id=%d' % app_id)
-
-        if app is None:
-            self.write(u'找不到该 APP')
-            return
+        self.set_header('Content-Type', 'text/json')
 
         if action == 'add':
-            pass
-        elif action == 'edit':
-            pass
+            qrcode_url = self.get_file_uploaded_url('qrcode_url')
+            if qrcode_url is None:
+                self.write(json.dumps(dict(flag=False, message='图片上传到七牛失败')))
+                return
 
+            bg_url = self.get_file_uploaded_url('bg_url')
+            if bg_url is None:
+                self.write(json.dumps(dict(flag=False, message='图片上传到七牛失败')))
+                return
+
+            sql = "INSERT INTO app(name, qrcode_url, bg_url, question, intro, input_label, " \
+                  "answer_prefix, uid, active) " \
+                  "VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', %d, %d)" \
+                  % (MySQLdb.escape_string(self.get_body_argument('name')),
+                     MySQLdb.escape_string(qrcode_url),
+                     MySQLdb.escape_string(bg_url),
+                     MySQLdb.escape_string(self.get_body_argument('question')),
+                     MySQLdb.escape_string(self.get_body_argument('intro')),
+                     MySQLdb.escape_string(self.get_body_argument('input_label')),
+                     '', 1, True)
+
+            db.insert(sql)
+            self.write(json.dumps(dict(flag=True, url='/test/admin/app')))
+
+        elif action == 'edit':
+            app_id = int(self.get_argument('app_id', 1))
+            app = db.get('SELECT * FROM app WHERE id=%d' % app_id)
+
+            if app is None:
+                self.write(json.dumps(dict(flag=False, message='未找到该 APP')))
+                return
+
+            qrcode_url = self.get_file_uploaded_url('qrcode_url')
+            if qrcode_url is None:
+                self.write(json.dumps(dict(flag=False, message='图片上传到七牛失败')))
+                return
+            elif qrcode_url == '':
+                qrcode_url = app.qrcode_url
+
+            bg_url = self.get_file_uploaded_url('bg_url')
+            if bg_url is None:
+                self.write(json.dumps(dict(flag=False, message='图片上传到七牛失败')))
+                return
+            elif bg_url == '':
+                bg_url = app.bg_url
+
+            sql = "UPDATE app SET name='%s', qrcode_url='%s', bg_url='%s', " \
+                  "question='%s', intro='%s', input_label='%s' WHERE id=%d" \
+                  % (MySQLdb.escape_string(self.get_body_argument('name')),
+                     MySQLdb.escape_string(qrcode_url),
+                     MySQLdb.escape_string(bg_url),
+                     MySQLdb.escape_string(self.get_body_argument('question')),
+                     MySQLdb.escape_string(self.get_body_argument('intro')),
+                     MySQLdb.escape_string(self.get_body_argument('input_label')),
+                     app_id)
+
+            db.update(sql)
+            self.write(json.dumps(dict(flag=True, url='/test/admin/app')))
+
+    def get_file_uploaded_url(self, name):
+        if name in self.request.files:
+            fileinfo = self.request.files[name][0]
+            fname = fileinfo['filename']
+            body = fileinfo['body']
+
+            extn = os.path.splitext(fname)[1]
+            cname = str(uuid.uuid4()) + extn
+
+            q = Auth(config.QINIU_AK, config.QINIU_SK)
+            key = 'upload/' + cname
+            token = q.upload_token(config.QINIU_BUCKET_NAME)
+            ret, info = put_data(token, key, body)
+
+            if info.status_code == 200:
+                return 'http://7xon5f.com2.z0.glb.qiniucdn.com/' + key
+            else:
+                return None
+
+        return ''
 
